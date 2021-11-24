d0956e541169a99857cc2b44c13e4d5fbb7ce15e
xiaoming lin <xiaominglin@xiaomingdeMacBook-Pro.local>
2021-11-24 09:52:50 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import sys
+
+print("Current python version:")
+print(sys.version)
+print()
+print("Current python path:")
+print(sys.executable)
+print()
+
+name = input("Please input your name: ")
+print("Hello, " + name + "!")
